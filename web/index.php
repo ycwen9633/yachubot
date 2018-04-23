f1f3cad383fdc11b5ef211a7732cf6e087a757fb
ycwen9633 <ycwen9633@gmail.com>
2018-04-23 18:29:13 +0800

    test: test9

--- web/index.php
+++ web/index.php
@@ -38,25 +38,26 @@ foreach ($client->parseEvents() as $event) {
             foreach ($data['feed']['entry'] as $item) {
                 $keywords = explode(',', $item['gsx$keyword']['$t']);
                 foreach ($keywords as $keyword) {
-                    $test_result = $keyword.$message['text'];
+                    
                     $keyword = iconv( "big5","UTF-8",  $keyword);
                     $message['text'] = iconv( "big5","UTF-8",  $message['text']);
-                    if (mb_strpos($message['text'], $keyword, 0, 'UTF8') !== false) {
-                        // $candidate = array(
-                        //     'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
-                        //     'title' => $item['gsx$title']['$t'],
-                        //     'text' => $item['gsx$title']['$t'],
-                        //     'actions' => array(
-                        //         array(
-                        //             'type' => 'uri',
-                        //             'label' => '查看詳情',
-                        //             'uri' => $item['gsx$url']['$t'],
-                        //             ),
-                        //         ),
-                        //     );
-                        $test_result = 'in';
-                        // array_push($result, $candidate);
-                    }
+                    $test_result = $keyword.$message['text'];
+                    // if (mb_strpos($message['text'], $keyword, 0, 'UTF8') !== false) {
+                    //     // $candidate = array(
+                    //     //     'thumbnailImageUrl' => $item['gsx$photourl']['$t'],
+                    //     //     'title' => $item['gsx$title']['$t'],
+                    //     //     'text' => $item['gsx$title']['$t'],
+                    //     //     'actions' => array(
+                    //     //         array(
+                    //     //             'type' => 'uri',
+                    //     //             'label' => '查看詳情',
+                    //     //             'uri' => $item['gsx$url']['$t'],
+                    //     //             ),
+                    //     //         ),
+                    //     //     );
+                    //     $test_result = 'in';
+                    //     // array_push($result, $candidate);
+                    // }
                 }
                 // $test_result = $keywords[0];
             }

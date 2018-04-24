8312445d50142a5da4c26ae01498c9307ed643fa
ycwen9633 <ycwen9633@gmail.com>
2018-04-24 15:46:26 +0800

    test: 18

--- web/index.php
+++ web/index.php
@@ -116,22 +116,32 @@ foreach ($client->parseEvents() as $event) {
                                 'messages' => array(
                                     array(
                                         'type' => 'template', // 訊息類型 (模板)
-                                        'altText' => '選項', // 替代文字
+                                        'altText' => 'Example buttons template', // 替代文字
                                         'template' => array(
                                             'type' => 'buttons', // 類型 (按鈕)
-                                            'thumbnailImageUrl' => 'https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/19145911_1557695574254612_6333142876660853306_n.jpg?_nc_cat=0&oh=31f76b4b75f3d94b8d8e1165b7c289df&oe=5B5378BA', // 圖片網址 <不一定需要>
-                                            'title' => '按下方按鈕可以快速對我提問喔:)',
-                                            'text' => '更詳細的操作說明在記事本！', // 文字
+                                            'thumbnailImageUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example.jpg', // 圖片網址 <不一定需要>
+                                            'title' => 'Example Menu', // 標題 <不一定需要>
+                                            'text' => 'Please select', // 文字
                                             'actions' => array(
                                                 array(
-                                                    'type' => 'message', 
-                                                    'label' => '我想更了解你', 
-                                                    'data' => '我想更了解你'
+                                                    'type' => 'postback', // 類型 (回傳)
+                                                    'label' => 'Postback example', // 標籤 1
+                                                    'data' => 'action=buy&itemid=123' // 資料
                                                 ),
-                                            ),
-                                        ),
-                                    ),
-                                ),
+                                                array(
+                                                    'type' => 'message', // 類型 (訊息)
+                                                    'label' => 'Message example', // 標籤 2
+                                                    'text' => 'Message example' // 用戶發送文字
+                                                ),
+                                                array(
+                                                    'type' => 'uri', // 類型 (連結)
+                                                    'label' => 'Uri example', // 標籤 3
+                                                    'uri' => 'https://github.com/GoneTone/line-example-bot-php' // 連結網址
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
                             ));
                         }
                     }

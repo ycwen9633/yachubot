7a22c3763a4899c0bbab757ee86baa02d54828a4
ycwen9633 <ycwen9633@gmail.com>
2018-04-24 19:07:48 +0800

    fix: text too long

--- web/index.php
+++ web/index.php
@@ -111,127 +111,43 @@ foreach ($client->parseEvents() as $event) {
                                     )
                                 )
                             ));
-                        } elseif (strpos($message['text'], '哲煜科技') !== false) {
+                        }  elseif (strpos($message['text'], '哲煜科技') !== false) {
                             $client->replyMessage(array(
                                 'replyToken' => $event['replyToken'],
                                 'messages' => array(
-                                    array(
-                                        'type' => 'template', // 訊息類型 (模板)
-                                        'altText' => '在哲煜科技的實習經歷', // 替代文字
-                                        'template' => array(
-                                            'type' => 'buttons', // 類型 (按鈕)
-                                            'text' => '公司主要以接案為主，開發過各式不同形象官網前後台以及TMS運輸管理系統，語言則以PHP為主，HTML、CSS、Javascript、Vue.js為輔，除了寫程式之外還需與專案經理溝通理解客戶需求。可以點選下方文字看我其他經歷喔', // 文字
-                                            'actions' => array(
-                                                array(
-                                                    'type' => 'message',
-                                                    'label' => '在富邦人壽核心系統部門擔任實習生', 
-                                                    'text' => '在富邦人壽核心系統部門擔任實習生'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在久元電子顧機台', 
-                                                    'text' => '在久元電子顧機台'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在大馬小吃擔任櫃台', 
-                                                    'text' => '在大馬小吃擔任櫃台'
-                                                )
-                                            )
-                                        )
+                                array(
+                                    'type' => 'text',
+                                    'text' => '公司主要以接案為主，開發過各式不同形象官網前後台以及TMS運輸管理系統，語言則以PHP為主，HTML、CSS、Javascript、Vue.js為輔，除了寫程式之外還需與專案經理溝通理解客戶需求。'
                                     )
                                 )
                             ));
-                        } elseif (strpos($message['text'], '富邦人壽') !== false) {
+                        }   elseif (strpos($message['text'], '富邦人壽') !== false) {
                             $client->replyMessage(array(
                                 'replyToken' => $event['replyToken'],
                                 'messages' => array(
-                                    array(
-                                        'type' => 'template', // 訊息類型 (模板)
-                                        'altText' => '在富邦人壽的實習經歷', // 替代文字
-                                        'template' => array(
-                                            'type' => 'buttons', // 類型 (按鈕)
-                                            'text' => '我在核心系統部門下的保費管理科擔任實習生，而我們科內主要負責的是保費支付的系統，在金融科技創新的席捲下，我很幸運的參與到保險業與Apple Pay支付接軌的專案，利用node.js串接Apple Pay API，幫忙測試保費由行動支付的可行性，與收單行溝通，瞭解行動支付的流程以及安全性，除了師傅指派的這項專案外，另與其他實習生一同發想創新提案以及舉辦UX桌遊競賽，在實習期間內自行報名了壽險考照班，對於保險的知識大大提升，除此之外亦了解到金融科技在實際金融業的發展程度。可以點選下方文字看我其他經歷喔', // 文字
-                                            'actions' => array(
-                                                array(
-                                                    'type' => 'message',
-                                                    'label' => '在哲煜科技擔任實習工程師', 
-                                                    'text' => '在哲煜科技擔任實習工程師'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在久元電子顧機台', 
-                                                    'text' => '在久元電子顧機台'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在大馬小吃擔任櫃台', 
-                                                    'text' => '在大馬小吃擔任櫃台'
-                                                )
-                                            )
-                                        )
+                                array(
+                                    'type' => 'text',
+                                    'text' => '我在核心系統部門下的保費管理科擔任實習生，而我們科內主要負責的是保費支付的系統，在金融科技創新的席捲下，我很幸運的參與到保險業與Apple Pay支付接軌的專案，利用node.js串接Apple Pay API，幫忙測試保費由行動支付的可行性，與收單行溝通，瞭解行動支付的流程以及安全性，除了師傅指派的這項專案外，另與其他實習生一同發想創新提案以及舉辦UX桌遊競賽，在實習期間內自行報名了壽險考照班，對於保險的知識大大提升，除此之外亦了解到金融科技在實際金融業的發展程度。'
                                     )
                                 )
                             ));
-                        } elseif (strpos($message['text'], '久元電子') !== false) {
+                        }   elseif (strpos($message['text'], '久元電子') !== false) {
                             $client->replyMessage(array(
                                 'replyToken' => $event['replyToken'],
                                 'messages' => array(
-                                    array(
-                                        'type' => 'template', // 訊息類型 (模板)
-                                        'altText' => '在哲煜科技的實習經歷', // 替代文字
-                                        'template' => array(
-                                            'type' => 'buttons', // 類型 (按鈕)
-                                            'text' => '在挑揀晶圓的無塵室中顧機台，處理機台發生的問題以及上下晶圓片，許多專業術語以及晶圓的型號等皆是全新的事物，需要非常專注的在短時間內記熟不同的機台對應的晶圓片以及藍膜，許多同批進去的工讀生幾天就辭職了，但對於喜歡嘗試新事物的我來說是一個全新的挑戰，而在經過努力學習後，我熟悉了挑檢晶圓作業的流程甚至對接續的封裝測試過程也有一定的瞭解，這份打工經驗驗證了我不願放棄、積極學習的態度，也讓自己更相信對於任何新事物都要保持著高度的好奇心。可以點選下方文字看我其他經歷喔', // 文字
-                                            'actions' => array(
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在哲煜科技擔任實習工程師', 
-                                                    'text' => '在哲煜科技擔任實習工程師'
-                                                ),
-                                                array(
-                                                    'type' => 'message',
-                                                    'label' => '在富邦人壽核心系統部門擔任實習生', 
-                                                    'text' => '在富邦人壽核心系統部門擔任實習生'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在大馬小吃擔任櫃台', 
-                                                    'text' => '在大馬小吃擔任櫃台'
-                                                )
-                                            )
-                                        )
+                                array(
+                                    'type' => 'text',
+                                    'text' => '在挑揀晶圓的無塵室中顧機台，處理機台發生的問題以及上下晶圓片，許多專業術語以及晶圓的型號等皆是全新的事物，需要非常專注的在短時間內記熟不同的機台對應的晶圓片以及藍膜，許多同批進去的工讀生幾天就辭職了，但對於喜歡嘗試新事物的我來說是一個全新的挑戰，而在經過努力學習後，我熟悉了挑檢晶圓作業的流程甚至對接續的封裝測試過程也有一定的瞭解，這份打工經驗驗證了我不願放棄、積極學習的態度，也讓自己更相信對於任何新事物都要保持著高度的好奇心。'
                                     )
                                 )
                             ));
-                        } elseif (strpos($message['text'], '大馬小吃') !== false) {
+                        }   elseif (strpos($message['text'], '大馬小吃') !== false) {
                             $client->replyMessage(array(
                                 'replyToken' => $event['replyToken'],
                                 'messages' => array(
-                                    array(
-                                        'type' => 'template', // 訊息類型 (模板)
-                                        'altText' => '在哲煜科技的實習經歷', // 替代文字
-                                        'template' => array(
-                                            'type' => 'buttons', // 類型 (按鈕)
-                                            'text' => '鄰近學校的餐廳，在尖峰時段非常忙碌，培養了我良好的危機處理以及應變的能力，期間內我不斷地提升自己的工作效能，讓出餐的流程更順暢、記得熟客點的品項，處事圓融的我也深受客人以及老闆的喜愛，面對不同的客人我能提供最貼合他們需求的服務。可以點選下方文字看我其他經歷喔', // 文字
-                                            'actions' => array(
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在哲煜科技擔任實習工程師', 
-                                                    'text' => '在哲煜科技擔任實習工程師'
-                                                ),
-                                                array(
-                                                    'type' => 'message',
-                                                    'label' => '在富邦人壽核心系統部門擔任實習生', 
-                                                    'text' => '在富邦人壽核心系統部門擔任實習生'
-                                                ),
-                                                array(
-                                                    'type' => 'message', 
-                                                    'label' => '在久元電子顧機台', 
-                                                    'text' => '在久元電子顧機台'
-                                                ),
-                                            )
-                                        )
+                                array(
+                                    'type' => 'text',
+                                    'text' => '鄰近學校的餐廳，在尖峰時段非常忙碌，培養了我良好的危機處理以及應變的能力，期間內我不斷地提升自己的工作效能，讓出餐的流程更順暢、記得熟客點的品項，處事圓融的我也深受客人以及老闆的喜愛，面對不同的客人我能提供最貼合他們需求的服務。'
                                     )
                                 )
                             ));
@@ -259,8 +175,8 @@ foreach ($client->parseEvents() as $event) {
                                                 ),
                                                 array(
                                                     'type' => 'message', 
-                                                    'label' => '在久元電子顧機台', 
-                                                    'text' => '在久元電子顧機台'
+                                                    'label' => '在九元電子顧機台', 
+                                                    'text' => '在九元電子顧機台'
                                                 ),
                                                 array(
                                                     'type' => 'message', 
